62b2cfbca70129aa16c5e3365afc5fca8a51c2d8
涂俊宏 <tujunhong0014@gmail.com>
2024-02-21 16:09:39 +0800

    123

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ *  @see https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  printWidth: 200,
+  tabWidth: 2,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  htmlWhitespaceSensitivity: 'ignore', // html 处理标签异常问题
+};
