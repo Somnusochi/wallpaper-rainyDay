7763aab2666eb09555b767a5646f0040e53d8a08
涂俊宏 <tujunhong0014@gmail.com>
2024-02-21 17:15:04 +0800

    chore: init

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
-      ],
-    ],
+    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
     'type-case': [0],
   },
 };
